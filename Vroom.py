50a4498141d5ac2d8b007d7fb51f7a843ffaf9e9
PastaBoi <fbrittingham04@gmail.com>
2019-08-21 13:03:14 -0400

    Made gameloop into a defintion

--- Vroom.py
+++ Vroom.py
@@ -35,50 +35,54 @@ clock = pygame.time.Clock()  # Creates the clock which is imposed on everything
 
 carImg = pygame.image.load('Car.png')  # Load Car.png
 
+
 #############
 # Functions #
 #############
-x = (displayWidth * 0.45)  # Set x position
-y = (displayHeight * 0.8)  # Set y position
-
 
 def car(x, y):  # Defines car
     gameDisplay.blit(carImg, (x, y))  # Blit Car.png on the display
     # 0,0 for computers is upper left. x = right y = down
 
 
-xChange = 0  # Sets default change value
+def game_loop():
+    x = (displayWidth * 0.45)  # Set x position
+    y = (displayHeight * 0.8)  # Set y position
 
-#############
-# Game Loop #
-#           #############################################
-# A 'Game Loop' is where all the game logic is located. #
-#########################################################
+    x_change = 0  # Sets default change value
+
+    #############
+    # Game Loop #
+    #           #############################################
+    # A 'Game Loop' is where all the game logic is located. #
+    #########################################################
+
+    crashed = False  # Default value crash is false (you have not crashed)
+    while not crashed:  # When your not crashed
+        for event in pygame.event.get():  # Retrieves any event and applies it like *
+            if event.type == pygame.QUIT:  # If the 'QUIT' event occurs the game ends
+                crashed = True  # Loop cease occurring
+                print(event)  # Print the event to console
+            if event.type == pygame.KEYDOWN:  # Check to see if some pressed a key
+                if event.key == pygame.K_LEFT:  # If the keypress if left arrow key
+                    x_change = -5  # Moves car 5 pixels to the left
+                elif event.key == pygame.K_RIGHT:  # Check to see if keypress is right arrow key
+                    x_change = +5  # Moves car five pixels to the right
+            if event.type == pygame.KEYUP:  # Check to see if key release
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:  # If the right / left arrows keys release
+                    x_change = 0  # Resets x so it is relative
+
+        x += x_change  # x = x + xChange // If xChange is -5 than x will move over minus five
+
+        gameDisplay.fill(white)  # Creates white background (put in right order or else get overwritten)
+        car(x, y)  # Runs car
+        pygame.display.update()  # Updates what the player sees
+        # pygame.display.update() = change just one thing in the parameter. If none specified than all updated
+        # pygame.display.flip() will always update all the surface
+        clock.tick(60)  # FPS is tied to clock so 60 tick = 60 FPS
 
 
-crashed = False  # Default value crash is false (you have not crashed)
-while not crashed:  # When your not crashed
-    for event in pygame.event.get():  # Retrieves any event and applies it like *
-        if event.type == pygame.QUIT:  # If the 'QUIT' event occurs the game ends
-            crashed = True  # Loop cease occurring
-            print(event)  # Print the event to console
-        if event.type == pygame.KEYDOWN:  # Check to see if some pressed a key
-            if event.key == pygame.K_LEFT:  # If the keypress if left arrow key
-                xChange = -5  # Moves car 5 pixels to the left
-            elif event.key == pygame.K_RIGHT:  # Check to see if keypress is right arrow key
-                xChange = +5  # Moves car five pixels to the right
-        if event.type == pygame.KEYUP:  # Check to see if key release
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:  # If the right / left arrows keys release
-                xChange = 0  # Resets x so it is relative
-
-    x += xChange  # x = x + xChange // If xChange is -5 than x will move over minus five
-
-    gameDisplay.fill(white)  # Creates white background (put in right order or else get overwritten)
-    car(x, y)  # Runs car
-    pygame.display.update()  # Updates what the player sees
-    # pygame.display.update() = change just one thing in the parameter. If none specified than all updated
-    # pygame.display.flip() will always update all the surface
-    clock.tick(60)  # FPS is tied to clock so 60 tick = 60 FPS
+game_loop()  # Run game loop
 
 ###############
 # End Process #
